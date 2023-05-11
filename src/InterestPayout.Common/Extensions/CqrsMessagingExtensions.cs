e116879f4d4dbdd7b9223338d8f940f2a4c8e366
Maksim Sustretov <maksim.sustretov@swisschain.io>
2023-05-11 16:36:35 +0300

    LM-2733 logging interceptor

--- src/InterestPayout.Common/Extensions/CqrsMessagingExtensions.cs
+++ src/InterestPayout.Common/Extensions/CqrsMessagingExtensions.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections.Generic;
 using InterestPayout.Common.Configuration;
 using InterestPayout.Common.Cqrs;
 using Lykke.Common.Log;
 using Lykke.Cqrs;
 using Lykke.Cqrs.Configuration;
+using Lykke.Cqrs.Configuration.Routing;
+using Lykke.Cqrs.Middleware.Logging;
 using Lykke.InterestPayout.MessagingContract;
 using Lykke.Messaging;
 using Lykke.Messaging.Contract;
@@ -19,15 +22,19 @@ namespace InterestPayout.Common.Extensions
         {
             serviceCollection.AddSingleton<IDependencyResolver, ServiceProviderDependencyResolver>();
 
+            if (string.IsNullOrWhiteSpace(rabbitMqConfig?.CqrsConnString))
+                throw new InvalidOperationException("RabbitMq.CqrsConnString is required.");
+            
+            var rabbitMqCqrsSettings = new RabbitMQ.Client.ConnectionFactory { Uri = new Uri(rabbitMqConfig.CqrsConnString) };
             serviceCollection.AddSingleton<IMessagingEngine>(c => new MessagingEngine(
                 c.GetService<ILogFactory>(),
                 new TransportResolver(new Dictionary<string, TransportInfo>
                 {
                     {
                         "RabbitMq",
-                        new TransportInfo(rabbitMqConfig.HostUrl,
-                            rabbitMqConfig.Username,
-                            rabbitMqConfig.Password,
+                        new TransportInfo(rabbitMqCqrsSettings.Endpoint.ToString(),
+                            rabbitMqCqrsSettings.UserName,
+                            rabbitMqCqrsSettings.Password,
                             "None",
                             "RabbitMq")
                     }
@@ -35,16 +42,23 @@ namespace InterestPayout.Common.Extensions
                 new RabbitMqTransportFactory(c.GetService<ILogFactory>())));
             
             const string environment = "lykke";
+            var rabbitMqConventionEndpointResolver =
+                new RabbitMqConventionEndpointResolver("RabbitMq",
+                    SerializationFormat.MessagePack,
+                    environment: environment);
+            
             serviceCollection.AddSingleton<ICqrsEngine>(c => new CqrsEngine(
                 c.GetService<ILogFactory>(),
                 c.GetService<IDependencyResolver>(),
                 c.GetService<IMessagingEngine>(),
                 new DefaultEndpointProvider(),
                 true,
+                Register.DefaultEndpointResolver(rabbitMqConventionEndpointResolver),
                 Register.DefaultEndpointResolver(new RabbitMqConventionEndpointResolver(
                     "RabbitMq",
                     SerializationFormat.MessagePack,
                     environment: environment)),
+                Register.EventInterceptors(new DefaultEventLoggingInterceptor(c.GetService<ILogFactory>())),
                 Register.BoundedContext(InterestPayoutBoundedContext.Name)
                     .PublishingEvents(
                         typeof (PayoutCompletedEvent)
